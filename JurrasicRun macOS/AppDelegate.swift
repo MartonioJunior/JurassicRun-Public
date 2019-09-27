5e79c8b662746f2165b1d73c309636882fc32ccf
Martonio Junior <martoniojunior@hotmail.com>
2019-09-27 08:29:23 -0300

    Initial Commit

---
+++ JurrasicRun macOS/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  JurrasicRun macOS
+//
+//  Created by Martônio Júnior on 27/09/19.
+//  Copyright © 2019 martonio. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
---
+++ JurrasicRun macOS/GameViewController.swift
@@ -0,0 +1,31 @@
+//
+//  GameViewController.swift
+//  JurrasicRun macOS
+//
+//  Created by Martônio Júnior on 27/09/19.
+//  Copyright © 2019 martonio. All rights reserved.
+//
+
+import Cocoa
+import SpriteKit
+import GameplayKit
+
+class GameViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let scene = GameScene.newGameScene()
+        
+        // Present the scene
+        let skView = self.view as! SKView
+        skView.presentScene(scene)
+        
+        skView.ignoresSiblingOrder = true
+        
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+    }
+
+}
+
